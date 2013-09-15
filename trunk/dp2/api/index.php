b2fc6f716df9fac0cfcb7c4c10d00d2f9da22937
abedoya@pucp.pe <abedoya@pucp.pe@7cc43834-62e3-2d65-d818-9159ef9c7cad>
2013-09-15 23:58:31 +0000

    links para la logica de negocios

--- trunk/dp2/api/index.php
+++ trunk/dp2/api/index.php
@@ -4,6 +4,10 @@ require 'Slim/Slim.php';
 $app = new \Slim\Slim();
  //Lógica de servicios
     include ("../backend/alcance/serviciosAlcance.php");
+    include ("../backend/presupuesto/serviciosPresupuesto.php");
+    include ("../backend/cronograma/serviciosCronograma.php");
+    include ("../backend/general/serviciosGeneral.php");
+    include ("../backend/riesgo/serviciosRiesgo.php");
  //fin
 $app->run();
 ?>
