a6c80656eb6519bfe8cb9e5be305479ee524db2d
b.carranza <b.carranza@pucp.pe>
2013-10-04 06:26:52 +0000

    asignar recursos humanos por proyecto

--- dp2/backend/general/routesGeneral2.php
+++ dp2/backend/general/routesGeneral2.php
@@ -1,26 +1,25 @@
 <?php
-    //GET to retrieve and search data
-    //POST to add data
-    //PUT to update data
-    //DELETE to delete data
+
+//GET to retrieve and search data
+//POST to add data
+//PUT to update data
+//DELETE to delete data
+//Bonnie -- lecciones aprendidas
+$app->get('/G_devuelveCategoria', 'G_getCategoria'); //devuelve las categorias de las lecciones aprendidas
+$app->post('/G_registrarLeccionAprendida', 'G_postRegistrarLeccionAprendida'); //registrar una leccion
+$app->get('/G_devuelveProyectosXEmpleado/:id', 'G_getProyectosXEmpleado'); //devuelve la lista de proyectos por empleado
+$app->post('/G_cambiaEstadoLeccionAprendida', 'G_postBorrarLeccionAprendida'); //borra (de modo logico) la leccion aprendida
+$app->post('/G_actualizarLeccionAprendida', 'G_postActualizarLeccionAprendida');
+$app->get('/G_devuelveLeccionesAprendidas', 'G_getLeccionesAprendidas'); //leer todas las lecciones aprendidas
+$app->get('/G_devuelveLeccionAprendidaById/:id', 'G_getLeccionAprendidasById');
 
 
-    //Bonnie -- lecciones aprendidas
-    $app->get('/G_devuelveCategoria', 'G_getCategoria'); //devuelve las categorias de las lecciones aprendidas
-    $app->post('/G_registrarLeccionAprendida', 'G_postRegistrarLeccionAprendida'); //registrar una leccion
-    $app->get('/G_devuelveProyectosXEmpleado/:id', 'G_getProyectosXEmpleado'); //devuelve la lista de proyectos por empleado
-    $app->post('/G_cambiaEstadoLeccionAprendida', 'G_postBorrarLeccionAprendida'); //borra (de modo logico) la leccion aprendida
-    $app->post('/G_actualizarLeccionAprendida', 'G_postActualizarLeccionAprendida');
-    $app->get('/G_devuelveLeccionesAprendidas', 'G_getLeccionesAprendidas'); //leer todas las lecciones aprendidas
-    $app->get('/G_devuelveLeccionAprendidaById/:id', 'G_getLeccionAprendidasById');
+$app->post('/G_asignarRecursoProyecto/:id', 'G_getAsignarRecProy');
+//jose -- RRHH
 
+$app->get('/G_listarRecursoDisponible', 'G_getListarRecDisp');
+$app->get('/G_listaRecursoxProyecto', 'G_getListaRecXProyecto');
 
-
-		//jose -- RRHH
-    $app->post('/G_asignarRecursoProyecto', 'G_getAsignarRecProy'); 
-    $app->get('/G_listarRecursoDisponible', 'G_getListarRecDisp'); 
-    $app->get('/G_listaRecursoxProyecto', 'G_getListaRecXProyecto'); 
-
-		//Alfonso -- Seguridad
-		$app->post('/G_verificaUsuario', 'G_getUsuario'); //devuelve 1 si el usuario y pass son correctas
+//Alfonso -- Seguridad
+$app->post('/G_verificaUsuario', 'G_getUsuario'); //devuelve 1 si el usuario y pass son correctas
 ?>
