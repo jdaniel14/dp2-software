c6e3a8c4e8d44adec206312ba262348e698b95dc
b.carranza <b.carranza@pucp.pe>
2013-10-19 21:55:23 +0000

    prueba

--- dp2/backend/general/serviciosGeneral3.php
+++ dp2/backend/general/serviciosGeneral3.php
@@ -139,7 +139,33 @@ function G_postCumpObjetivosPorProyecto() {
     }
 }
 
-
+function G_getCostoPorProyecto($id) {
+    //$sql = " SELECT id_objetivo, descripcion, comentarios, flag_cumplido FROM OBJETIVO where id_proyecto=:id";
+    try {
+//        $db = getConnection();
+//        $stmt = $db->prepare($sql);
+//        $stmt->bindParam("id", $id);
+//        $stmt->execute();
+//
+//        $l_objetivos = array();
+//        while ($j = $stmt->fetch(PDO::FETCH_ASSOC)) {
+//            $obj = array(
+//                "id" => $j["id_objetivo"],
+//                "desc" => $j["descripcion"]
+//            );
+//            array_push($l_objetivos, $obj);
+//        }
+//        $db = null;
+        
+        $costos = array(
+                "c_est" => "25",
+                "c_real" => "20"
+            );
+        echo json_encode(array("costos" => $costos));
+    } catch (PDOException $e) {
+        echo json_encode(array("me" => $e->getMessage()));
+    }
+}
 
 
 ?>
