75ac1c1ce56010f9e818dd00f8c659d99a81cb13
b.carranza@pucp.pe <b.carranza@pucp.pe@7cc43834-62e3-2d65-d818-9159ef9c7cad>
2013-10-19 21:55:23 +0000

    prueba

--- trunk/dp2/backend/general/routesGeneral3.php
+++ trunk/dp2/backend/general/routesGeneral3.php
@@ -6,6 +6,7 @@ $app->get('/G_devuelveValidarCierreProyecto/:id', 'G_getValidarSuccess');
 $app->get('/G_listarObjetivosPorProyecto/:id', 'G_getObjetivosPorProyecto');
 $app->post('/G_registrarObjetivosPorProyecto', 'G_postObjetivosPorProyecto');
 $app->post('/G_actualizarCumpObjetivosPorProyecto', 'G_postCumpObjetivosPorProyecto');
+$app->get('/G_devuelveCostoPorProyecto/:id', 'G_getCostoPorProyecto');
 
 
 //
