37ea4b1497ffa5c08daf534f0c6b73c6b3a51fc0
b.carranza <b.carranza@pucp.pe>
2013-10-20 00:37:00 +0000

    prueba

--- dp2/backend/general/routesGeneral3.php
+++ dp2/backend/general/routesGeneral3.php
@@ -7,6 +7,7 @@ $app->get('/G_listarObjetivosPorProyecto/:id', 'G_getObjetivosPorProyecto');
 $app->post('/G_registrarObjetivosPorProyecto', 'G_postObjetivosPorProyecto');
 $app->post('/G_actualizarCumpObjetivosPorProyecto', 'G_postCumpObjetivosPorProyecto');
 $app->get('/G_devuelveCostoPorProyecto/:id', 'G_getCostoPorProyecto');
+$app->get('/G_devuelveCostoPorProyectoPrueba/:id', 'G_getCostoPorProyectoPrueba');
 
 
 //
--- dp2/backend/general/serviciosGeneral3.php
+++ dp2/backend/general/serviciosGeneral3.php
@@ -141,7 +141,7 @@ function G_postCumpObjetivosPorProyecto() {
     }
 }
 
-function G_getCostoPorProyecto($id) {
+function G_getCostoPorProyectoPrueba($id) {
     //$sql = " SELECT id_objetivo, descripcion, comentarios, flag_cumplido FROM OBJETIVO where id_proyecto=:id";
     try {
 //        $db = getConnection();
@@ -184,6 +184,109 @@ function G_getCostoPorProyecto($id) {
     } catch (PDOException $e) {
         echo json_encode(array("me" => $e->getMessage()));
     }
+}
+
+function G_getCostoPorProyecto($id) {
+    //obtener paquete raíz
+    $sql = "SELECT
+    A.ID_PROYECTO,
+    Y.ID_PAQUETE_TRABAJO,
+    Y.NOMBRE NOMBRE_PAQUETE,
+    0 as COSTO_PAQUETE_SOLES
+    from 
+    PROYECTO A 
+    JOIN EDT Z ON A.ID_PROYECTO=Z.ID_PROYECTO
+    JOIN PAQUETE_TRABAJO Y ON Z.ID_EDT=Y.ID_EDT
+    WHERE
+    Y.ID_COMPONENTE_PADRE IS NULL 
+    AND
+    A.ID_PROYECTO= :idProyecto AND Z.ID_ESTADO<>4
+    GROUP BY
+    A.ID_PROYECTO,
+    Y.ID_PAQUETE_TRABAJO,
+    Y.NOMBRE ";
+
+  
+    $l_costos_edt = array();
+    $costo_total_real = 0;
+    $costo_total_est = 0;
+    
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("idProyecto", $id);
+        $stmt->execute();
+
+        while ($p = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            //id paquete, nombre paquete, lista de paquetes hijo
+            $idPaqueteRaiz = $p["ID_PAQUETE_TRABAJO"];
+
+            $sql = "SELECT
+			A.ID_PROYECTO,
+			Y.ID_PAQUETE_TRABAJO,
+			Y.NOMBRE NOMBRE_PAQUETE,
+			EST.descripcion as ESTADO,
+			SUM(CASE
+			WHEN B.ID_PAQUETE_TRABAJO IS NULL OR C.ID_ACTIVIDAD IS NULL OR D.ID_RECURSO IS NULL OR X.ID_CAMBIO_MONEDA IS NULL THEN 0
+			WHEN B.PROFUNDIDAD<>0 AND B.ELIMINADO<>1  AND Z.ID_ESTADO<>4 AND D.ESTADO<>'ELIMINADO' AND C.ESTADO<>0
+				THEN IFNULL(C.CANTIDADESTIMADA,0)*(IFNULL(D.COSTO_UNITARIO_ESTIMADO*CAMBIO_A_SOL,0))
+			ELSE 0	
+			END
+			) COSTO_PAQUETE_SOLES
+			from 
+			ESTADO_EDT EST,
+			PROYECTO A 
+			JOIN EDT Z ON A.ID_PROYECTO=Z.ID_PROYECTO
+			JOIN PAQUETE_TRABAJO Y ON Z.ID_EDT=Y.ID_EDT
+			LEFT JOIN ACTIVIDAD B ON Y.ID_PAQUETE_TRABAJO=B.ID_PAQUETE_TRABAJO
+			LEFT JOIN ACTIVIDAD_X_RECURSO C ON B.ID_ACTIVIDAD=C.ID_ACTIVIDAD
+			LEFT JOIN RECURSO D ON C.ID_RECURSO=D.ID_RECURSO
+			LEFT JOIN CAMBIO_HISTORICO X ON D.ID_CAMBIO_MONEDA=X.ID_CAMBIO_MONEDA
+			WHERE
+			Y.ID_COMPONENTE_PADRE= :idPaquete AND (X.FECHA IS NULL OR DATE_FORMAT(X.FECHA,'%Y%m%d')=DATE_FORMAT(SYSDATE(),'%Y%m%d')) and EST.id_estado = Y.id_estado
+			GROUP BY
+			A.ID_PROYECTO,
+			Y.ID_PAQUETE_TRABAJO,
+			Y.NOMBRE; ";
+
+            try {
+                $stmt = $db->prepare($sql);
+                $stmt->bindParam("idPaquete", $idPaqueteRaiz);
+                $stmt->execute();
+
+                while ($p = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    //id paquete, nombre paquete, lista de paquetes hijo
+                    $c_real = $p["COSTO_PAQUETE_SOLES"];
+                    $c_est = $p["COSTO_PAQUETE_SOLES"];
+                    
+                    $paqueteHijo = array(
+                        "nombre" => $p["NOMBRE_PAQUETE"],
+                        "estado" => $p["ESTADO"],
+                        "c_est" => (string)$c_est,
+                        "c_real" => (string)$c_real
+                    );
+                    $costo_total_real += $c_real;
+                    $costo_total_est += $c_est;
+                    array_push($l_costos_edt, $paqueteHijo);
+                }
+            } catch (PDOException $e) {
+                $db=null;
+                echo json_encode(array("me" => $e->getMessage()));
+                return;
+            }
+        }
+        $costos = array(
+            "c_total_est" => (string) $costo_total_est,
+            "c_total_real" => (string) $costo_total_real,
+            "l_costos_edt" => $l_costos_edt
+        );
+        echo json_encode(array("costos" => $costos));
+    } catch (PDOException $e) {
+        $db=null;
+        echo json_encode(array("me" => $e->getMessage()));
+        return;
+    }
+    $db = null;
 }
 
 ?>
